5a4ea0b6d40fe2c12b1b02e3980c1fa8b918c63c
Olimar Teixeira B <olimarbjunior@gmail.com>
2020-07-12 16:26:33 -0300

    Versão atualizada, com uso de proxys para a busca dos H5 das conferências.

--- scriptPythonConfs_cp.py
+++ scriptPythonConfs_cp.py
@@ -3,9 +3,14 @@ import pickle
 import os.path
 import re
 import pandas as pd
+
+import requests
+import time
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import proxyscrape
+from proxyscrape import create_collector, get_collector
 
 from datetime import datetime
 import urllib3
@@ -22,15 +27,13 @@ SAMPLE_RANGE_NAME = 'Qualis!A1:J1572'
 
 SERVICE_ACCOUNT_FILE = 'credencialContaServico.json'
 
-credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 def main():
-    print('1/6. Iniciando o processo de configuração e autenticação com o Google Sheets, para buscar TODOS os dados da planilha.')
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
+    print('1/5. Iniciando o processo de configuração e autenticação com o Google Sheets, para buscar TODOS os dados da planilha.')
 
-    delegated_credentials = credentials.with_subject('[credencial_omitida]')
+    delegated_credentials = credentials.with_subject('[CREDENCIAL_OMITIDA]')
     service = build('sheets', 'v4', credentials=delegated_credentials)
     # Call the Sheets API
     sheet = service.spreadsheets()
@@ -51,82 +54,106 @@ def main():
             ds = pd.Series(data=column_data, name=col_name)
             all_data.append(ds)
         df = pd.concat(all_data, axis=1)
-    print('Finalizou: 1/6')
+    print('Finalizou: 1/5')
 
-    print('2/6. Iniciando o processo de busca do Qualis...')
-    valores = realizaParanaue(df)
-    print('Finalizou: 2/6')
+    terminou = False
+    row = df.shape[0] #1571
+    val_init = 0
+    val_fin = 99
+    collector = make_collector("https://www.google.com/")
+    http = create_new_session(collector)
+    itr = 1
+    print('###### Realiza o processo a cada 100 conferências, divido em iterações:')
+    while terminou == False:
+        if val_fin > row:
+            val_fin = row
+            terminou = True
+        print('2/5. Iniciando o processo de busca do Qualis... - Iteração: ', itr)
+        print('Iteração: ============ ', itr)
 
-    print('3/6. Iniciando o processo de configuração e autenticação novamente com o Google Sheets, para buscar ALGUNS dados da planilha para fazer o UPDATE.')
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    print('Finalizou: 3/6')
+        df_new = df.iloc[val_init:val_fin]
+        print('Prim.Conf ', df_new.iloc[[0], 0])
+        print('Últ.Conf ', df_new.iloc[[-1], 0])
 
-    print('4/6. Iniciando a configuração para realizar o Update na planilha.')
-    #print(valores)
+        valores, http = realizaParanaue(df_new, http)
 
-    # The A1 notation of the values to update. 'Qualis!F2:G2'
-    range_update = 'Qualis!F2:J1572'  # TODO: Update placeholder value.
+        print('Finalizou: 2/5 - Iteração: ', itr)
 
-    # How the input data should be interpreted.
-    value_input_option = 'USER_ENTERED'  # TODO: Update placeholder value.
+        print('3/5. Iniciando a configuração para realizar o Update na planilha. - Iteração: ', itr)
 
-    value_range_body = {
-        "values": valores
-    }
-    #print(value_range_body)
-    print('Finalizou: 4/6')
-    print('5/6. Iniciando ação para efetivar o update na planilha.')
+        if val_init == 0:
+            linhas_init = val_init+2
+            linhas_fin = val_fin+1
+            val_init+=99
+        else:
 
-    request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=range_update,
-                                    valueInputOption=value_input_option,
-                                    body=value_range_body).execute()
+            linhas_init = val_init+2
+            linhas_fin = val_fin+1
+            val_init+=100
 
-    print('Finalizou: 5/6')
-    print('6/6. Planilha atualizada com sucesso!')
-    print('{0} cells updated.'.format(request.get('updatedCells')))
+        val_fin+=100
 
+        # The A1 notation of the values to update. 'Qualis!F2:G2'
+        range_update = 'Qualis!F'+ str(linhas_init) +':J'+str(linhas_fin)  # TODO: Update placeholder value.
 
-def realizaParanaue(df):
+        # How the input data should be interpreted.
+        value_input_option = 'USER_ENTERED'  # TODO: Update placeholder value.
+
+        value_range_body = {
+            "values": valores
+        }
+
+        print('Finalizou: 3/5 - Iteração: ', itr)
+        print('4/5. Iniciando ação para efetivar o update na planilha.', itr)
+
+        request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                        range=range_update,
+                                        valueInputOption=value_input_option,
+                                        body=value_range_body).execute()
+
+        print('Finalizou: 4/5 - Iteração: ', itr)
+        itr+=1
+
+    print('5/5. Planilha atualizada com sucesso!', itr)
+
+def realizaParanaue(df, http):
     qualis = None
     estratoBase = None
     valores = []
-    row = df.shape[0]
+    #row = df.shape[0]
 
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
-    with Bar('Processando', max=row) as bar:
-        for index in range(0, row):
-            linha = []
-            #toda vez que o script executar, ele vai registrar a data desta tentativa de atualização
-            linha.insert(3, dt_string)
-            if df.loc[index, 'categoria'] != '3' or df.loc[index, 'categoria'] != '4':
+    for index, row in df.iterrows():
+        linha = []
+        #toda vez que o script executar, ele vai registrar a data desta tentativa de atualização
+        linha.insert(3, dt_string)
+
+        categoria = df.loc[index, 'categoria']
+        if categoria != '3' and categoria != '4':
+
+            if 'tag span' not in df.loc[index, 'logs']:
+
                 site = df.loc[index, 'link']
                 if site != 'nulo':
-                    #print(df.loc[index, 'sigla'])
-                    h5, log = buscaH5(site)
+                    h5, log, http = buscaH5(site, http)
                     if log != '': #deu erro ao acessar o site
-                        print('_log: ', log)
                         linha.insert(0, df.loc[index, 'h5'])
                         linha.insert(1, df.loc[index, 'Qualis_Final'])
                         linha.insert(2, df.loc[index, 'data-atualizacao'])
                         linha.insert(4, log)
                     elif h5 != None:
-                        print('_h5: ', h5)
                         linha.insert(0, h5)
                         linha.insert(2, dt_string)
                         if h5 != 0 :
                             estratoBase = aplicaRegra(h5)
                             if df.loc[index, 'categoria'] == '1':
                                 qualis = estratoBase
-                                #atualiza valor na célula do qualis desta linha
                                 linha.insert(1, qualis)
-                            elif df.loc[0, 'categoria'] == '2':
-                                valorTop = df.loc[index, 'CE Indicou']
+                                linha.insert(4, 'atualizado com sucesso')
+                            elif df.loc[index, 'categoria'] == '2':
+                                valorTop = df.loc[index, 'CE_Indicou']
                                 qualis = validaCategoria2(valorTop, estratoBase)
                                 linha.insert(1, qualis)
                                 linha.insert(4, 'atualizado com sucesso')
@@ -150,40 +177,81 @@ def realizaParanaue(df):
                 linha.insert(0, df.loc[index, 'h5'])
                 linha.insert(1, df.loc[index, 'Qualis_Final'])
                 linha.insert(2, df.loc[index, 'data-atualizacao'])
-                linha.insert(4, 'categorias 3 e 4 não são atualizadas')
+                linha.insert(4, df.loc[index, 'logs'])
+        else:
+            linha.insert(0, df.loc[index, 'h5'])
+            linha.insert(1, df.loc[index, 'Qualis_Final'])
+            linha.insert(2, df.loc[index, 'data-atualizacao'])
+            linha.insert(4, 'categorias 3 e 4 não são atualizadas')
 
-            valores.insert(index, linha)
-            bar.next()
+        valores.insert(index, linha)
 
-    return valores
+    return valores, http
 
 # executa busca no google
-def buscaH5(site):
+def buscaH5(site, http):
+
     h5index = None
     log = ''
-    http = urllib3.PoolManager()
     try:
-        response = http.request('GET', site)
+        http_collector = make_collector(site)
+
+        page, http = get_page(site, http, http_collector)
     except urllib3.exceptions.HTTPError as e:
-        #print(e)
         log = 'link do site inacessível: ' + str(e)
+    except proxyscrape.errors.CollectorAlreadyDefinedError as e:
+        log = 'link é o mesmo utilizado para outro evento: ' + str(e)
     else:
-        res = BeautifulSoup(response.data, 'html.parser')
+        content = page.content
+        res = BeautifulSoup(content, 'html.parser')
         tags = res.findAll("ul", {"class": "gsc_mlhd_list"}, "span")
         try:
             h5index = tags[0].span
         except IndexError as a:
-            log = 'não encontrou tag span: ' + str(a)
-    #deixa somente o número, tirando as tags html
+            log = 'não encontrou a tag span: ' + str(a)
+
     h5index = re.sub('[^0-9]', '', str(h5index))
 
     if h5index == '':
         h5index = None
     else:
-        #base 10 transforma o valor numérico da str em decimal
         h5index = int(h5index, 10)
 
-    return h5index, str(log)
+    return h5index, str(log), http
+
+def get_page(url, http, http_collector):
+    page = requests.get("https://www.google.com/")
+    while True:
+        try:
+            page = http.get(url, timeout=6)
+            if page.status_code == 200:
+                break
+        except BaseException as error:
+            pass
+        finally:
+            if page.url == "https://www.google.com/" or page.status_code != 200:
+                http.proxies['https']
+                http = setup_new_proxies(http_collector, http)
+
+    return page, http
+
+def make_collector(page_i=''):
+        http_collector = create_collector(f'http-collector-{page_i}', 'https')
+        return http_collector
+
+def setup_new_proxies(http_collector, http):
+     proxy_http = http_collector.get_proxy()
+     proxy_https = http_collector.get_proxy({'type':'https'})
+     http.proxies={
+        'http': f'http://{proxy_http.host}:{proxy_http.port}',
+        'https' : f'https://{proxy_https.host}:{proxy_https.port}'
+     }
+     return http
+
+def create_new_session(http_collector):  # precisa chamar
+    http = requests.Session()
+    http = setup_new_proxies(http_collector, http)
+    return http
 
 def aplicaRegra(h5):
     estratoBase = None
@@ -245,6 +313,7 @@ def sobeNivel(estratoBase, nivel):
             qualis = "B2"
 
     return qualis
+
 
 if __name__ == '__main__':
     main()
